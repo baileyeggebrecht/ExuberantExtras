dcca8eef0d23c51ee4fdf922a5785af99631f041
BaileyEggebrecht <baileyteggebrecht@gmail.com>
2022-04-13 17:13:51 -0600

    Everything Else

--- shopping-cart.php
+++ shopping-cart.php
@@ -1,13 +1,21 @@
-<?php
-
-session_start();
-
-var_dump($_SESSION['cart']);
-
-$whereIn = implode(',', $_SESSION['cart']);
-
-$sql = "
-    SELECT * FROM products
-    WHERE id IN (3,4,5)
-
-";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-eqiuv="X-UA-Compatible" content="ie=edge">
+    <title>Shopping Cart</title>
+    
+    <!---Font Awesome----->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+    
+    <!---Bootstrap.cdn---->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    </head>
+    <body>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+    </body>
+</html>
---
+++ validation.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+
+$con = mysqli_connect('localhost', 'root');
+
+mysqli_select_db($con, 'EEUserRegistration');
+
+$name = $_POST['user'];
+$pass = $_POST['password'];
+
+$s = " select * from UserTable where name = '$name' && password = '$pass'";
+
+$result = mysqli_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+    echo" Username Already Taken";
+}else{
+    $reg= " INSERT INTO UserTable(name , password) VALUES ('$name','$pass')";
+    mysqli_query($con, $reg);
+    echo" Registration Successful";
+}
+?>
